eaf4abdf561d6babb765cd9839b17bc8b00039e4
ranawp <ranasaha4224#gmail.com>
2022-05-09 09:41:06 +0600

    make api of all inventory

--- index.js
+++ index.js
@@ -26,12 +26,7 @@ async function run() {
             res.send(inventories)
         })
 
-        app.get('/inventory/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) }
-            const service = await serviceCollection.findOne(query)
-            res.send(service)
-        })
+
 
     }
     finally {
